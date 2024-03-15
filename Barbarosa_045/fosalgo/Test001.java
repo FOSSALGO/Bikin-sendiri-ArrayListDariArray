8c2f6c41b927b67969dc552ee0a2e50d6e83370c
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2024-03-15 09:19:54 +0800

    Create Test001.java

---
+++ Barbarosa_045/fosalgo/Test001.java
@@ -0,0 +1,18 @@
+package fosalgo;
+
+import java.util.Arrays;
+
+public class Test001 {
+    public static void main(String[] args) {
+        Object[] data = new Object[12];        
+        data[0] = 1267;
+        data[1] = 351.276;
+        data[7] = 7423;
+        data[5] = 98531;
+        data[8] = "Iwan Firgiawan";   
+        data[11] = 46.21;
+        data[2] = "Far if";
+        
+        System.out.println(Arrays.toString(data));
+    }
+}
