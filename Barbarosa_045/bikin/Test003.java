6d7eb0d453cfab2fed701511048aa6a318ac7a5a
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2024-03-15 09:19:08 +0800

    Create Test003.java

---
+++ Barbarosa_045/bikin/Test003.java
@@ -0,0 +1,49 @@
+package bikin;
+
+public class Test003 {
+    public static void main(String[] args) {
+        ArrayList data = new ArrayList();
+        System.out.println(data.isEmpty());
+        System.out.println(data.size());
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        data.add(4);
+        data.add(5);
+        data.add(6);
+        data.add(7);
+        data.add(8);
+        data.add(9);
+        data.add(10);
+        data.add(11);
+        data.add(12);
+        data.add(31);
+        data.add(8);
+        data.add(9);
+        data.add(10);
+        data.add(11);
+        data.add(12);
+        data.add(31);
+        data.add(9);
+        data.add(10);
+        data.add(11);
+        data.add(12);
+        data.add(31);
+        data.add(8);
+        data.add(9);
+        data.add(10);
+        data.add(11);
+        data.add(9);
+        data.add(10);
+        data.add(11);
+        data.add(12);
+        data.add(31);
+        data.add(8);
+        data.add(9);
+        data.add(10);
+        data.add(11);
+        data.add(12);
+        data.add(31);
+        System.out.println(data);
+    }
+}
