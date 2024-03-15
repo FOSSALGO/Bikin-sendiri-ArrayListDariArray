eb6f52af85dbd972b9742fb3fed2ef2bf3e70383
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2024-03-15 09:22:57 +0800

    Create Test004.java

---
+++ Barbarosa_045/test/Test004.java
@@ -0,0 +1,12 @@
+package test;
+
+import bikin.ArrayList;
+
+public class Test004 {
+    public static void main(String[] args) {
+        ArrayList data = new ArrayList();
+        System.out.println(data.isEmpty());
+        System.out.println(data.size());
+        
+    }
+}
